0fafab2cb4fe38c61bf0601d4acf5ff54c3b4edb
BorisovaDaria <anton.kravtsov.31@gmail.com>
2022-01-31 21:18:54 +0300

    team adding func + 2 test teams

--- src/db.py
+++ src/db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import itertools
 
 bd = sqlite3.connect('../data/data.db')
 cursor = bd.cursor()
@@ -14,15 +15,72 @@ def commit():
     global bd
     bd.commit()
 
+def get_teams_list():
+    cursor.execute("SELECT Name FROM Teams")
+    teams = list(itertools.chain(*cursor.fetchall()))
+    return teams
+
+def get_heroes_list():
+    cursor.execute("SELECT Name FROM Heroes")
+    heroes = list(itertools.chain(*cursor.fetchall()))
+    return heroes
+
+
+def get_heroes_of_position(position):
+    cursor.execute("SELECT Name FROM Heroes WHERE Positions LIKE ?", ('%' + position + '%',))
+    return list(itertools.chain(*cursor.fetchall()))
+
+
+def add_player(team, nick, country, fight, farm, teamwork, signatures, position):
+    cursor.execute("SELECT Id FROM Teams WHERE Name=:name", {"name": team})
+    team_id = cursor.fetchone()[0]
+
+    to_players = (nick, country, team_id, position)
+    cursor.execute("""INSERT INTO Players(Nickname, Country, TeamId, Position)
+                      VALUES(?, ?, ?, ?)""", to_players)
+
+    cursor.execute("SELECT Id FROM Players WHERE Nickname=:nick", {"nick": nick})
+    player_id = cursor.fetchone()[0]
+
+    to_player_stat = (fight, farm, teamwork, signatures, team_id, player_id)
+    cursor.execute("""INSERT INTO PlayerStats(Fight, Farm, Teamwork, Signatures, TeamId, PlayerId)
+                      VALUES(?, ?, ?, ?, ?, ?)""", to_player_stat)
+
+    commit()
+
+
+def add_coach(team, nick, country, skill, morale, communication):
+    cursor.execute("SELECT Id FROM Teams WHERE Name=:name", {"name": team})
+    team_id = cursor.fetchone()[0]
+
+    to_coaches = (nick, country, team_id)
+    cursor.execute("""INSERT INTO Coaches(Nickname, Country, TeamId)
+                      VALUES(?, ?, ?)""", to_coaches)
+
+    cursor.execute("SELECT Id FROM Coaches WHERE Nickname=:name", {"name": nick})
+    coach_id = cursor.fetchone()[0]
+
+    to_coach_stats = (skill, morale, communication, team_id, coach_id)
+    cursor.execute("""INSERT INTO CoachStats(Skill, Morale, Communication, TeamId, CoachId)
+                      VALUES(?, ?, ?, ?, ?)""", to_coach_stats)
+
+    commit()
+
+
+def add_team(name, country):
+    to_teams = (name, country)
+    cursor.execute("""INSERT INTO Teams(Name, Country)
+                      VALUES(?, ?)""", to_teams)
+
+    commit()
+
 
 def create_hero(name, positions, fight_k, farm_k, teamwork_k):
-    init()
-
     to_heroes = (name, positions)
     cursor.execute("""INSERT INTO Heroes(Name, Positions)
                       VALUES(?, ?)""", to_heroes)
 
-    cursor.execute("""SELECT Id FROM Heroes WHERE Name='""" + name + """'""")
+    cursor.execute("""SELECT Id FROM Heroes WHERE Name=:name""", {"name": name})
     hero_id = cursor.fetchone()[0]
 
     to_hero_stats = (hero_id, fight_k, farm_k, teamwork_k)
@@ -32,5 +90,6 @@ def create_hero(name, positions, fight_k, farm_k, teamwork_k):
     commit()
 
 
-cursor.close()
-bd.close()
+def end_connection():
+    cursor.close()
+    bd.close()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,49 @@ import random as r
 from colorama import *
 
 
+def create_team():
+    team_name = input("Team name: ")
+    team_country = input("Team country (country code): ")
+    nicknames = input("Nicknames (space between, coach last, [core, core, core, semi-sup, sup, coach]): ").split()
+    players_countries = input("Countries (space between): ").split()
+
+    if len(nicknames) != 6 or len(players_countries) != 6:
+        print(Fore.RED + "[Wrong player or country number]" + Fore.RESET)
+        return
+
+    print("Adding team to database")
+    db.add_team(team_name, team_country)
+    print(Fore.GREEN + "[Team added successfully]" + Fore.RESET)
+
+    print("Adding players to database")
+    skill_sum = 0
+    positions = ["Core", "Core", "Core", "Semi-Sup", "Sup"]
+    for i in range(5):
+        nick = nicknames[i]
+        country = players_countries[i]
+        position = positions[i]
+        fight = r.randint(0, 100)
+        farm = r.randint(0, 100)
+        teamwork = r.randint(0, 100)
+        skill_sum += fight + farm + teamwork
+        heroes_of_position = db.get_heroes_of_position(position)
+        signatures = ','.join(r.choices(heroes_of_position, k=3))
+
+        db.add_player(team_name, nick, country, fight, farm, teamwork, signatures, position)
+
+    # adding coach
+    print("Adding coach to database")
+    nick = nicknames[5]
+    country = players_countries[5]
+    skill = r.randint(0, 100)
+    morale = r.randint(0, 100)
+    communication = r.randint(0, 100)
+
+    db.add_coach(team_name, nick, country, skill, morale, communication)
+
+    print(Fore.GREEN + "[Players and Coach added successfully]" + Fore.RESET)
+
+
 def create_hero():
     name = input("Hero name: ")
     print("[Write positions like this: Core, Semi-Sup, Sup]")
@@ -33,13 +76,48 @@ def create_hero():
     print(Fore.GREEN + "[Successfully added]" + Fore.RESET)
 
 
+def print_hero_list():
+    print(db.get_heroes_list())
+
+
+def print_hero_list_by_position(inp: str):
+    if len(inp.split()) != 2:
+        print(Fore.RED + "[Wrong argument count]" + Fore.RESET)
+        return
+    print(db.get_heroes_of_position(inp.split()[1]))
+
+
+def print_team_list():
+    print(db.get_teams_list())
+
+def start_quick_match():
+    
+
 def main():
+    db.init()
     print("Welcome to DotaSim2 v0")
     while True:
         user_input = input(">> ")
 
         if user_input == "ch":
             create_hero()
+
+        elif user_input == "ct":
+            create_team()
+
+        elif user_input == "hl":
+            print_hero_list()
+
+        elif user_input.split()[0] == 'hlp':
+            print_hero_list_by_position(user_input)
+
+        elif user_input == "tl":
+            print_team_list()
+
+        elif user_input == "q":
+            db.end_connection()
+            break
+
 
 if __name__ == '__main__':
     main()
